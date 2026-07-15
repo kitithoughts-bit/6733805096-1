2512711b268e6b0d19ca64af71d9327f9e90ea5c
Vacharapoln <Vacharapoln.d@kkumail.com>
2026-07-15 13:59:16 +0700

    feat: update student info - ID 6733805096 sec 1

---
+++ myproject6733805096/src/main/java/com/example/myproject6733805096/Myproject6733805096Application.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733805096;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject6733805096Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject6733805096Application.class, args);
+	}
+
+}
---
+++ myproject6733805096/src/test/java/com/example/myproject6733805096/Myproject6733805096ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733805096;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject6733805096ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
